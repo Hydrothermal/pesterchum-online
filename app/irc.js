bc29e96d1dda33cd84d523f77f5f7de9d2b5911c
Hydrothermal <hydrothermal@tuta.io>
2016-06-30 00:58:01 -0400

    Fixed blank nicks getting through

--- app/irc.js
+++ app/irc.js
@@ -124,7 +124,7 @@ module.exports = {
         config = _config;
     },
     validateNick: function(nick) {
-        return /^[a-z0-9\\[\]\^_\-|`]{1,30}$/i.test(nick);
+        return !!nick && /^[a-z0-9\\[\]\^_\-|`]{1,30}$/i.test(nick);
     },
     createClient: createClient
 };
