a91c67f296f7b889838ed1821b8918921aee9e52
Hydrothermal <hydrothermal@tuta.io>
2017-05-01 22:00:58 -0400

    Added nick menu and history

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,5 @@
+var nicks;
+
 function error() {
     $("#error").animate({ opacity: 1 }, 150);
 
@@ -7,7 +9,38 @@ function error() {
     }
 }
 
+function getNicks() {
+    nicks = localStorage.getItem("nicks");
+
+    if(!nicks) {
+        localStorage.setItem("nicks", "");
+        nicks = [];
+    } else {
+        nicks = nicks.split(";");
+    }
+}
+
+function loadNicks() {
+    for(var i = 0; i < nicks.length; i++) {
+        $("#nick-menu").append("<div class='nick'>" + nicks[i] + "</div>")
+    }
+}
+
+function addNick(nick) {
+    var i = nicks.indexOf(nick);
+
+    if(i === -1) {
+        nicks.unshift(nick);
+    } else {
+        nicks.unshift(nicks.splice(i, 1)[0]);
+    }
+    
+    localStorage.setItem("nicks", nicks.join(";"));
+}
+
 $(function() {
+    loadNicks();
+
     $("#nick-input").focus();
 
     $("#nick").submit(function() {
@@ -17,9 +50,28 @@ $(function() {
             error();
             return false;
         }
+
+        addNick(nick);
     });
+
+    $("#nick-menuicon").click(function() {
+        $(this).toggleClass("is-active");
+        $("#nick-input").toggleClass("open");
+        $("#nick-menu").slideToggle(150);
+    });
+
+    $("#nick-menu .nick").click(function() {
+        $("#nick-input").val(this.innerHTML);
+        $("#nick-menuicon").click();
+    });
+
+    if(nicks.length === 0) {
+        $("#nick-menuicon").hide();
+    }
 
     if(document.URL.indexOf("?err") > -1) {
         error();
     }
 });
+
+getNicks();
